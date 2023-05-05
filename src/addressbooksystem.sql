2940456cb9d3fc6c7e82e6b53ee0da00d75d902c
milindkumar64 <milindgupta@gmail.com>
2023-05-05 13:44:42 +0530

    Add: create AddressBook table and show

--- src/addressbooksystem.sql
+++ src/addressbooksystem.sql
@@ -1,4 +1,12 @@
 show databases;
 ---- UC1 ----
 create database address_book_service;
-use address_book_service;
+use address_book_services
+
+---- UC2 ----
+create table address_book(id int not null auto_increment, first_name varchar(50) not null, 
+last_name varchar(50) not null, address varchar(200) not null, city varchar(50) not null, 
+state varchar(50) not null, zip int not null,  phone_number long not null, 
+email varchar(50) not null, primary key(id)); 
+
+describe address_book;
