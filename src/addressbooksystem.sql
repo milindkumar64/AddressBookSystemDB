6e2181efdc927e149ed2264b49f1bf04fdc82b23
milindkumar64 <milindgupta@gmail.com>
2023-05-05 14:01:04 +0530

    Add: insert data into AddressBook table and show

--- src/addressbooksystem.sql
+++ src/addressbooksystem.sql
@@ -10,3 +10,18 @@ state varchar(50) not null, zip int not null,  phone_number long not null,
 email varchar(50) not null, primary key(id)); 
 
 describe address_book;
+
+---- UC3 ----
+insert into address_book(first_name,last_name,address,city,state,zip,phone_number,email) values 
+('Saras','Kumar','sirmour chock','Rewa','MP',486001,1234567890,'saras@hotmail.com');
+
+insert into address_book(first_name,last_name,address,city,state,zip,phone_number,email) values 
+('Narendra','Modi','7 RCR Road','Delhi','Delhi',123456,123454568,'pmmodi@govin.com');
+
+insert into address_book(first_name,last_name,address,city,state,zip,phone_number,email) values 
+('Yogi','adityanath','CM awash','Lucknow','UP',654321,767878089,'cmup@govup.com');
+
+insert into address_book(first_name,last_name,address,city,state,zip,phone_number,email) values 
+('Nitin','Gadkari','LIC chock','Nagpur','MP',145789,741443202,'nitin@govin.com');
+
+Select * from address_book;
